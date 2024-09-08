6445782d40a0cbf1319b33079950ab701ea57297
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-09-09 08:45:26 +0900

    続き

--- backend/v1/index.php
+++ backend/v1/index.php
@@ -1,19 +1,138 @@
 <?php
+// require_once('database/database.php');
+// require_once('controller/controllers.php');
+// require_once('rooter.php');
+// require_once('constant.php');
 
 
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 
-require_once('common/database.php');
-require_once('controller/controllers.php');
-require_once('rooter.php');
-require_once('constant.php');
+$resource = array_shift($request);
+$id = array_shift($request);
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+switch ($resource) {
+    case 'read-histories':
+        handleReadHistories($method, $id, $data);
+        break;
+    case 'books':
+        handleBooks($method, $id, $data);
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(["message" => "Resource not found"]);
+        break;
+}
+
+function handleReadHistories($method, $id, $data)
+{
+    switch ($method) {
+        case 'GET':
+            if ($id) {
+                // IDを指定した履歴の取得
+                getReadHistory($id);
+            } else {
+                // 全履歴の取得
+                getAllReadHistories();
+            }
+            break;
+        case 'PUT':
+            if ($id && $data) {
+                // 指定した履歴の修正
+                updateReadHistory($id, $data);
+            }
+            break;
+        case 'DELETE':
+            if ($id) {
+                // 指定した履歴の削除
+                deleteReadHistory($id);
+            }
+            break;
+        default:
+            http_response_code(405);
+            echo json_encode(["message" => "Method not allowed"]);
+            break;
+    }
+}
+
+function handleBooks($method, $id, $data)
+{
+    switch ($method) {
+        case 'GET':
+            if ($id) {
+                // IDを指定した本の情報の取得
+                getBook($id);
+            } else {
+                // 全ての本の情報の取得
+                getAllBooks();
+            }
+            break;
+        case 'POST':
+            if ($data) {
+                // 新しい本の情報を追加
+                addBook($data);
+            }
+            break;
+        default:
+            http_response_code(405);
+            echo json_encode(["message" => "Method not allowed"]);
+            break;
+    }
+}
+
+// 履歴の取得関数（サンプル実装）
+function getReadHistory($id)
+{
+    echo json_encode(["message" => "Fetching read history with ID $id"]);
+}
+
+// 全履歴の取得関数（サンプル実装）
+function getAllReadHistories()
+{
+    echo json_encode(["message" => "Fetching all read histories"]);
+}
+
+// 履歴の修正関数（サンプル実装）
+function updateReadHistory($id, $data)
+{
+    echo json_encode(["message" => "Updating read history with ID $id", "data" => $data]);
+}
+
+// 履歴の削除関数（サンプル実装）
+function deleteReadHistory($id)
+{
+    echo json_encode(["message" => "Deleting read history with ID $id"]);
+}
+
+// 本の情報の取得関数（サンプル実装）
+function getBook($id)
+{
+    echo json_encode(["message" => "Fetching book with ID $id"]);
+}
+
+// 全ての本の情報の取得関数（サンプル実装）
+function getAllBooks()
+{
+    echo json_encode(["message" => "Fetching all books"]);
+}
+
+// 新しい本の情報を追加する関数（サンプル実装）
+function addBook($data)
+{
+    echo json_encode(["message" => "Adding new book", "data" => $data]);
+}
+
+
+
+/*
 if (json_last_error() !== JSON_ERROR_NONE) {
     error_log('JSON Error: ' . json_last_error_msg());
     echo "Error";
-    exit; // 処理を中断
+    exit;
 }
+*/
 
 //$data['user_sign_up'] = [];
 //$data['user_sign_up']['user_sign_up_user'] = '田中';
@@ -30,7 +149,7 @@ if (json_last_error() !== JSON_ERROR_NONE) {
 //$data[RegCust] = [];
 //$data[RegCust][RegCustId] = 'asdads';
 //$data[RegCust][RegCustName] = 'マツダ株式会社';
-
+/*
 $response = [];
 
 try {
@@ -57,3 +176,4 @@ $router->addRoute(ProgReg, new ProgramRegistrationController($db));
 $response = $router->dispatch($data);
 echo json_encode($response);
 $db = null;
+*/
