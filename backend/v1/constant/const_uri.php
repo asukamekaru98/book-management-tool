c0016d9aabe4e65bfd891f67ad7241211b18b734
asuka.m2298oki@gmail.com <mH9Z+EG4>
2024-09-23 23:54:39 +0900

    uriのdefineをすべてconst化

--- backend/v1/constant/const_uri.php
+++ backend/v1/constant/const_uri.php
@@ -1,23 +1,21 @@
 <?php
-
 # URI
 // 本棚
-define('URI_BOOK_SHELF', 'book-shelf');
+const URI_BOOK_SHELF = 'book-shelf';
 // ほしいものリスト
-define('URI_WISH_LIST', 'wish-list');
+const URI_WISH_LIST = 'wish-list';
 // 読書履歴
-define('URI_READ_HIST', 'read-histories');
+const URI_READ_HIST = 'read-histories';
 
 # Query
 // ISBN
-define('URI_QUERY_ISBN', 'isbn');
-define('URI_QUERY_DATA_FORMAT', 'format');
-
+const URI_QUERY_ISBN = 'isbn';
+const URI_QUERY_DATA_FORMAT = 'format';
 
 # Query設定値
-define('URI_QUERY_DATA_FORMAT_JSON', 'json');
-define('URI_QUERY_DATA_FORMAT_XML', 'xml');
+const URI_QUERY_DATA_FORMAT_JSON = 'json';
+const URI_QUERY_DATA_FORMAT_XML = 'xml';
 
 # Query初期値
-define('INIT_URI_QUERY_ISBN', '0000000000000');
-define('INIT_URI_QUERY_DATA_FORMAT', URI_QUERY_DATA_FORMAT_JSON);
+const INIT_URI_QUERY_ISBN = '0000000000000';
+const INIT_URI_QUERY_DATA_FORMAT = URI_QUERY_DATA_FORMAT_JSON;
