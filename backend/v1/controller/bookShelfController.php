5f456d6d1caa02c11fb3b834d63ca6d07312822e
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-09-29 15:07:19 +0900

    getAllBooksInfo

--- backend/v1/controller/bookShelfController.php
+++ backend/v1/controller/bookShelfController.php
@@ -22,7 +22,7 @@ class bookShelfController extends resourceController
             }
         } else {
             // 全履歴の取得
-            // getAllBooksInfo();
+            $book =  getAllBooksInfo();
         }
     }
 
@@ -73,6 +73,51 @@ class bookShelfController extends resourceController
                     LEFT JOIN books
                     ON books.id = books_shelf.book_id
                     WHERE books.isbn = '$isbn'
+                    EOD;
+
+            $stm = $this->db->prepare($sql);
+            $stm->execute();
+            $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            http_response_code(VARIANT_ALSO_NEGOTIATES_506);
+        }
+
+        http_response_code(OK_200);
+
+        $responseJSON = [
+            'bookinfo' => [
+                'isbn' => $result['isbn'],
+                'title' => $result['title'],
+                'sub_title' => $result['sub_title'],
+                'author' => $result['author'],
+                'description' => $result['description'],
+                'image_url' => $result['image_url'],
+                'published_date' => $result['published_date'],
+                'content' => $result['content'],
+            ],
+            'userinfo' => [
+                'industry_important' => $result['industry_important'],
+                'work_important' => $result['work_important'],
+                'user_important' => $result['user_important'],
+                'priority' => $result['priority'],
+                'purchased_flag' => $result['purchased_flag'],
+                'viewed_flag' => $result['viewed_flag'],
+            ],
+        ];
+
+        echo json_encode($result);
+    }
+
+    function getAllBooksInfo()
+    {
+        try {
+            //self::$db->beginTransaction();
+
+            $sql = <<< "EOD"
+                    SELECT books.isbn,books.title,books.sub_title,books.author,books.description,books.page,books.image_url,books.published_date,books.content,books.industry_important,books.work_important,books.user_important,books.priority,books.purchased_flag,books.viewed_flag
+                    FROM books_shelf
+                    LEFT JOIN books
+                    ON books.id = books_shelf.book_id
                     EOD;
 
             $stm = $this->db->prepare($sql);
