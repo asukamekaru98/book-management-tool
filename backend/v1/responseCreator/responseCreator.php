61010cebac3e35f612cf09611de3d05def4baba5
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-10-21 18:07:01 +0900

    ResponseCreator コンストラクタの初期値設定。多分使わないと思う

--- backend/v1/responseCreator/responseCreator.php
+++ backend/v1/responseCreator/responseCreator.php
@@ -7,6 +7,7 @@ use Interfaces\I_ResponseBodyCreator;
 use Interfaces\I_ResponseCodeCreator;
 
 use ResponseCodeCreator\ResponseCodeCreator;
+use ResponseBodyCreator\RespBodyCre8er;
 
 require_once __DIR__ . '/../interfaces/i_responseBodyCreator.php';
 require_once __DIR__ . '/../interfaces/i_responseCodeCreator.php';
@@ -17,7 +18,7 @@ class ResponseCreator implements I_ResponseCreator
 	private string $returnBody;
 
 	public function __construct(
-		protected I_ResponseBodyCreator $respBodyCreator,
+		protected I_ResponseBodyCreator $respBodyCreator = new RespBodyCre8er(),
 		protected I_ResponseCodeCreator $respCodeCreator = new ResponseCodeCreator()
 	) {}
 
