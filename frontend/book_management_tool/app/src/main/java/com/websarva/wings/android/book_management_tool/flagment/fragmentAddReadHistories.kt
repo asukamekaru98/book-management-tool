be9b6c92dd3f79e0c641c04d403692e87f23d813
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-11-24 22:41:21 +0900

    readhistories 途中

--- frontend/book_management_tool/app/src/main/java/com/websarva/wings/android/book_management_tool/flagment/fragmentAddReadHistories.kt
+++ frontend/book_management_tool/app/src/main/java/com/websarva/wings/android/book_management_tool/flagment/fragmentAddReadHistories.kt
@@ -1,60 +1,109 @@
 package com.websarva.wings.android.book_management_tool.flagment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import com.websarva.wings.android.book_management_tool.R
+import com.websarva.wings.android.book_management_tool.api.BookManagementToolAPIManager
+import com.websarva.wings.android.book_management_tool.apiBody.BmtAPIReadHistoriesRequestBodyCreator
+import com.websarva.wings.android.book_management_tool.apiBody.BmtAPIWishListRequestBodyCreator
+import com.websarva.wings.android.book_management_tool.constants.BookManagementToolApiMaxLength
+import com.websarva.wings.android.book_management_tool.databinding.FragmentAddWishListBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragmentAddReadHistories.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragmentAddReadHistories : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
-		}
-	}
+	private lateinit var binding: FragmentAddWishListBinding
+
+	val industryImportant = binding.root.findViewById<Spinner>(R.id.spinnerIndustryImportant).selectedItem.toString()
+	val workImportant = binding.root.findViewById<Spinner>(R.id.spinnerWorkImportant).selectedItem.toString()
+	val userImportant = binding.root.findViewById<Spinner>(R.id.spinnerUserImportant).selectedItem.toString()
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_add_read_histories, container, false)
+	): View {
+		binding = FragmentAddWishListBinding.inflate(inflater, container, false)
+
+		this.setupSpinners() // スピナーの設定
+		this.setupRegisterButton() // 登録ボタンの設定
+		this.setupEditText() // 入力ボックスの設定
+
+
+		return binding.root
 	}
 
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of
-		 * this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment fragmentAddReadHistories.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic
-		fun newInstance(param1: String, param2: String) =
-			fragmentAddReadHistories().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
+}/**
+ * 登録ボタンが押された時の処理
+ */
+private fun onClickRegisterButton() {
+
+	val isbnCode = binding.root.findViewById<EditText>(R.id.isbn_code_edit_text).text.toString()
+	val memo = binding.root.findViewById<EditText>(R.id.memo_edit_text).text.toString()
+
+	if(isbnCode.length != 13) {
+		Toast.makeText(requireContext(), "13文字入力してください", Toast.LENGTH_SHORT).show()
+		return
+	}
+
+	if(memo.length > BookManagementToolApiMaxLength.MAX_LENGTH_WISH_LIST_MEMO) {
+		val message = "メモは${BookManagementToolApiMaxLength.MAX_LENGTH_WISH_LIST_MEMO}文字以内で入力してください"
+
+		Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+		return
+	}
+
+
+
+	val body = BmtAPIReadHistoriesRequestBodyCreator(
+		importantToNumString(industryImportant),
+		importantToNumString(workImportant),
+		importantToNumString(userImportant),
+		"0",
+		"0",
+		"",
+		"",
+		"",
+		memo,
+		"",
+	)
+
+	Log.d("BookMgmtTool Button Click Result", body.get())
+
+	CoroutineScope(Dispatchers.Main).launch {
+		try {
+			withContext(Dispatchers.IO) {
+				BookManagementToolAPIManager().addOneWishList(isbnCode, body.get())
 			}
+			Toast.makeText(requireContext(), "登録しました", Toast.LENGTH_SHORT).show()
+
+		} catch (e: Exception) {
+
+			// 409エラーの場合は、すでに登録されている旨のメッセージを表示
+			if (e.message.toString().contains("409")) {
+				Toast.makeText(requireContext(), "すでに登録されています", Toast.LENGTH_SHORT).show()
+			} else {
+				Toast.makeText(requireContext(), "エラーが発生しました", Toast.LENGTH_SHORT).show()
+			}
+
+
+			Log.e(
+				"BookMgmtTool Exception",
+				e.message.toString() + "/" + e.stackTraceToString() + "/" + e.cause.toString()
+			)
+		}
+
+		//Log.d("BookMgmtTool Button Click Result", bookData.message)
+
 	}
 }
